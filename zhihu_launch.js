153f460f962ad1fb17c81dbd4c5753f2ed4c36d1
justceo <8763306@gmail.com>
2020-11-07 20:30:13 +0800

    Create zhihu_launch.js

---
+++ zhihu_launch.js
@@ -0,0 +1,11 @@
+/**
+ * MitM: 113.59.43.66, api.zhihu.com
+ * ^https://api\.zhihu\.com/commercial_api/launch_v2 url script-response-body zhihu/zhihu_launch.js
+ * 
+ */
+
+ const launch = JSON.stringify({ ads: [] })
+ 
+ console.log('\n拦截知乎 launch AD');
+
+ $done({ body: JSON.stringify({ launch }) });
