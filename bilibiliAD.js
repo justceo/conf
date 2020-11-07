7d568c9d2600d62c325ed70860d387aeff362de5
justceo <8763306@gmail.com>
2020-11-07 20:25:52 +0800

    Update bilibiliAD.js

--- bilibiliAD.js
+++ bilibiliAD.js
@@ -1,5 +1,6 @@
 /**
  * 去除 bilibili 开屏广告
+ ^https:\/\/app.bilibili.com\/x\/v2\/splash\/list\?.* url script-response-body https://raw.githubusercontent.com/justceo/conf/main/bilibiliAD.js
  */
 
 let obj = JSON.parse($response.body);
